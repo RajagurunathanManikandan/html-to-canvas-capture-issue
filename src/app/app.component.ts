4bc094f9f4b9b430c25d16a0c3561f640b613831
RajagurunathanManikandan <rajagurunathan.manikandan@kovai.co>
2022-02-19 02:40:01 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 import jsPDF from 'jspdf';
-import * as htmlToImage from "html-to-image";
+import { iterator } from 'rxjs/internal/symbol/iterator';
+//import * as htmlToImage from "html-to-image";
 
 @Component({
   selector: 'my-app',
@@ -9,31 +10,43 @@ import * as htmlToImage from "html-to-image";
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  totalIterator:number[] = [];
+
+  ngOnInit()
+  {
+    let iterator:number = 1;
+    while(iterator <= 100)
+    {
+      this.totalIterator.push(iterator);
+      iterator++;
+    }
+  }
 
   downloadPdf(){
-    const element = document.getElementById("download-dashboard");
+    const element = document.getElementById("download-pdf");
     element.setAttribute("style", "overflow: initial !important;");
+    console.log(element.childNodes);
     
-    htmlToImage.toCanvas(element).then((canvas) => {
-      var imgWidth = 210;
-      var pageHeight = 295;
-      var imgHeight = (canvas.height * imgWidth) / canvas.width;
-      var heightLeft = imgHeight;
+    // htmlToImage.toCanvas(element).then((canvas) => {
+    //   var imgWidth = 210;
+    //   var pageHeight = 295;
+    //   var imgHeight = (canvas.height * imgWidth) / canvas.width;
+    //   var heightLeft = imgHeight;
 
-      var pdf = new jsPDF("p", "mm");
-      var position = 0;
+    //   var pdf = new jsPDF("p", "mm");
+    //   var position = 0;
 
-      pdf.addImage(canvas.toDataURL(), "PNG", 0, position, imgWidth, imgHeight);
-      heightLeft -= pageHeight;
-      while (heightLeft >= 0) {
-        position = heightLeft - imgHeight;
-        pdf.addPage();
-        pdf.addImage(canvas.toDataURL(), "PNG", 0, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-      }
-      pdf.save("DemoPDF.pdf");
+    //   pdf.addImage(canvas.toDataURL(), "PNG", 0, position, imgWidth, imgHeight);
+    //   heightLeft -= pageHeight;
+    //   while (heightLeft >= 0) {
+    //     position = heightLeft - imgHeight;
+    //     pdf.addPage();
+    //     pdf.addImage(canvas.toDataURL(), "PNG", 0, position, imgWidth, imgHeight);
+    //     heightLeft -= pageHeight;
+    //   }
+    //   pdf.save("DemoPDF.pdf");
       
-      element.removeAttribute("style");
-    });
+    //   element.removeAttribute("style");
+    // });
   }
 }
